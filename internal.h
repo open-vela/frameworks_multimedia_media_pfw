bd0c5adf4f7d57238ed45e0a0978950228f9afe2
jihandong <jihandong@xiaomi.com>
2023-05-19 17:17:34 +0800

    frameworks/pfw: first version
    Signed-off-by: jihandong <jihandong@xiaomi.com>

---
+++ internal.h
@@ -0,0 +1,253 @@
+/****************************************************************************
+ * pfw/internal.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef PFW_INTERNAL_H
+#define PFW_INTERNAL_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include "pfw.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <sys/queue.h>
+
+/****************************************************************************
+ * Pre-Processor Definations
+ ****************************************************************************/
+
+#if CONFIG_LIB_PFW_DEBUG
+#define PFW_DEBUG(...) printf(__VA_ARGS__)
+#else
+#define PFW_DEBUG(...)
+#endif
+#define PFW_INFO(...) printf(__VA_ARGS__)
+
+/* Criterion types. */
+
+#define PFW_CRITERION_EXCLUSIVE 1 // Enum, each value has a literal meaning.
+#define PFW_CRITERION_INCLUSIVE 2 // Bitmask, each bit has a literal meaning.
+#define PFW_CRITERION_NUMERICAL 3 // Pure 32-bit integer.
+
+/* Rule pridecates. */
+
+#define PFW_PREDICATE_ALL 1 // True if all branches are true.
+#define PFW_PREDICATE_ANY 2 // True if any branch is true.
+#define PFW_PREDICATE_IS 3 // True if qeual to.
+#define PFW_PREDICATE_ISNOT 4 // True if not equal to.
+#define PFW_PREDICATE_INCLUDES 5 // True if has this bit.
+#define PFW_PREDICATE_EXCLUDES 6 // True if dose not has this bit.
+#define PFW_PREDICATE_IN 7 // True if in the interval.
+#define PFW_PREDICATE_NOTIN 8 // // True if not in the interval.
+
+/****************************************************************************
+ * Types
+ ****************************************************************************/
+
+typedef struct pfw_context_t pfw_context_t;
+typedef struct pfw_vector_t pfw_vector_t;
+typedef struct pfw_interval_t pfw_interval_t;
+typedef struct pfw_criterion_t pfw_criterion_t;
+typedef struct pfw_rule_t pfw_rule_t;
+typedef struct pfw_act_t pfw_act_t;
+typedef struct pfw_action_t pfw_action_t;
+typedef struct pfw_config_t pfw_config_t;
+typedef struct pfw_domain_t pfw_domain_t;
+typedef struct pfw_listener_t pfw_listener_t;
+typedef LIST_HEAD(pfw_listener_list_t, pfw_listener_t)
+    pfw_listener_list_t;
+typedef struct pfw_plugin_t pfw_plugin_t;
+typedef struct pfw_system_t pfw_system_t;
+
+/**
+ * @brief pfw_interval_t is a open interval, used by NumericalCriterion.
+ * @see pfw_criterion_t
+ */
+struct pfw_interval_t {
+    int32_t left;
+    int32_t right;
+};
+
+/**
+ * @brief pfw_criterion_t is condition variable.
+ *
+ * When criterion is modified, the rules in domains might change;
+ * so domain might change in next apply.
+ *
+ * @see pfw_rule_t pfw_apply() pfw_criterion_*()
+ */
+struct pfw_criterion_t {
+    int type; // @see PFW_CRITERION_*
+    pfw_vector_t* names;
+    pfw_vector_t* ranges;
+    int32_t state;
+    union {
+        const char* def;
+        int32_t v;
+    } init;
+};
+
+/**
+ * @brief pfw_rule_t is tree.
+ *
+ * Branch node is a composite of multiple rules; leaf node
+ * requires a criterion to be a specific state.
+ *
+ * @see pfw_criterion_t pfw_config_t
+ */
+struct pfw_rule_t {
+    int predicate; // @see PFW_PREDICATE_*
+    pfw_vector_t* branches;
+    union {
+        const char* def;
+        pfw_criterion_t* p;
+    } criterion;
+    union {
+        int32_t v;
+        const char* def;
+        pfw_interval_t* itv;
+    } state;
+};
+
+/**
+ * @brief pfw_act_t is applying paramter by plugin method.
+ *
+ * Apply the parameter to all listener callbacks in the plugin
+ * when the act is applied.
+ *
+ * @see pfw_action_t pfw_plugin_t
+ */
+struct pfw_act_t {
+    const char* params;
+    union {
+        const char* def;
+        pfw_plugin_t* p;
+    } plugin;
+};
+
+/**
+ * @brief pfw_config_t is a state in state machine.
+ *
+ * Once the rules is ok, take acts.
+ *
+ * @see pfw_domain_t pfw_rule_t pfw_action_t
+ */
+struct pfw_config_t {
+    const char* name;
+    pfw_rule_t* rules;
+    pfw_vector_t* acts;
+};
+
+/**
+ * @brief pfw_domain_t is a state machine.
+ *
+ * when apply changes, state machine might move into a new state.
+ *
+ * @see pfw_config_t pfw_apply()
+ */
+struct pfw_domain_t {
+    const char* name;
+    pfw_config_t* current;
+    pfw_vector_t* configs;
+};
+
+/**
+ * @brief pfw_listener_t is a callback in plugin
+ * @see pfw_plugin_t
+ */
+struct pfw_listener_t {
+    void* cookie;
+    pfw_callback_t cb;
+    LIST_ENTRY(pfw_listener_t)
+    entry;
+};
+
+/**
+ * @brief pfw_plugin_t is a sequence of callback
+ *
+ * Callbacks used by act in state machine, user can add/remove callback
+ * to any plugin at runtime.
+ *
+ * @see pfw_listener_t pfw_subscribe() pfw_unsubscribe()
+ */
+struct pfw_plugin_t {
+    char* name;
+    pfw_listener_list_t listeners;
+};
+
+/**
+ * @brief pfw_system_t is a system that includes condition variables and
+ * state machines.
+ *
+ * Create system by parsing two configuration files, regist plugin,
+ * and initialize the criteria and domains.
+ *
+ * @see pfw_criterion_t pfw_domain_t pfw_context_t
+ * pfw_create() pfw_destroy()
+ */
+struct pfw_system_t {
+    pfw_context_t* criteria_ctx;
+    pfw_context_t* settings_ctx;
+    pfw_vector_t* criteria;
+    pfw_vector_t* domains;
+    pfw_plugin_t* plugins;
+    int nb_plugins;
+    pfw_load_t load; // Load criterion state at initilization.
+    pfw_save_t save; // Save criterion state when it changes.
+};
+
+/****************************************************************************
+ * Functions
+ ****************************************************************************/
+
+/* Utils functions. */
+
+pfw_context_t* pfw_context_create(const char* filename);
+char* pfw_context_take_word(pfw_context_t* ctx);
+char* pfw_context_take_line(pfw_context_t* ctx);
+int pfw_context_get_depth(pfw_context_t* ctx);
+void pfw_context_destroy(pfw_context_t* ctx);
+
+int pfw_vector_append(pfw_vector_t** pv, void* obj);
+void* pfw_vector_get(pfw_vector_t* vector, int index);
+void pfw_vector_free(pfw_vector_t* vector);
+
+/* Parse functions. */
+
+void pfw_free_criteria(pfw_vector_t* criteria);
+void pfw_free_settings(pfw_vector_t* settings);
+int pfw_parse_criteria(pfw_context_t* ctx, pfw_vector_t** p);
+int pfw_parse_settings(pfw_context_t* ctx, pfw_vector_t** p);
+
+bool pfw_sanitize_criteria(pfw_system_t* system);
+bool pfw_sanitize_settings(pfw_system_t* system);
+
+/* Criterion functions */
+
+bool pfw_rule_match(pfw_rule_t* rule);
+int pfw_criterion_atoi(pfw_criterion_t* criterion,
+    const char* value, int32_t* state);
+int pfw_criterion_itoa(pfw_criterion_t* criterion,
+    int32_t state, char* res, int len);
+pfw_criterion_t* pfw_criteria_find(pfw_vector_t* criteria,
+    const char* target);
+
+#endif // PFW_INTERNAL_H
---
+++ parser.c
@@ -0,0 +1,559 @@
+/****************************************************************************
+ * pfw/parser.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include "internal.h"
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+static pfw_interval_t* pfw_parse_interval(const char* word)
+{
+    pfw_interval_t* itv;
+
+    itv = malloc(sizeof(pfw_interval_t));
+    if (!itv)
+        return NULL;
+
+    if (2 == sscanf(word, "[%d,%d]", &itv->left, &itv->right))
+        ;
+    else if (1 == sscanf(word, "[%d,]", &itv->left))
+        itv->right = INT32_MAX;
+    else if (1 == sscanf(word, "[,%d]", &itv->right))
+        itv->left = INT32_MIN;
+    else
+        itv->left = itv->right = strtol(word, NULL, 0);
+
+    return itv;
+}
+
+static void pfw_free_rule(pfw_rule_t* rule)
+{
+    pfw_rule_t* sub;
+    int i;
+
+    if (!rule)
+        return;
+
+    for (i = 0; (sub = pfw_vector_get(rule->branches, i)); i++)
+        pfw_free_rule(sub);
+
+    if (rule->predicate == PFW_PREDICATE_IN
+        || rule->predicate == PFW_PREDICATE_NOTIN)
+        free(rule->state.itv);
+
+    pfw_vector_free(rule->branches);
+    free(rule);
+}
+
+static void pfw_free_config(pfw_config_t* config)
+{
+    pfw_act_t* act;
+    int i;
+
+    pfw_free_rule(config->rules);
+    for (i = 0; (act = pfw_vector_get(config->acts, i)); i++)
+        free(act);
+
+    pfw_vector_free(config->acts);
+    free(config);
+}
+
+static void pfw_free_domain(pfw_domain_t* domain)
+{
+    pfw_config_t* config;
+    int i;
+
+    for (i = 0; (config = pfw_vector_get(domain->configs, i)); i++)
+        pfw_free_config(config);
+
+    pfw_vector_free(domain->configs);
+    free(domain);
+}
+
+static void pfw_free_criterion(pfw_criterion_t* criterion)
+{
+    pfw_interval_t* intervel;
+    int i;
+
+    if (!criterion)
+        return;
+
+    if (criterion->type == PFW_CRITERION_NUMERICAL) {
+        for (i = 0; (intervel = pfw_vector_get(criterion->ranges, i)); i++)
+            free(intervel);
+    }
+
+    pfw_vector_free(criterion->ranges);
+    pfw_vector_free(criterion->names);
+    free(criterion);
+}
+
+static int pfw_parse_rule(pfw_context_t* ctx, pfw_rule_t** pr, int depth)
+{
+    pfw_rule_t *rule, *sub;
+    char* word;
+    bool branch;
+    int ret, nb;
+
+    ret = pfw_context_get_depth(ctx);
+    if (ret != depth)
+        return ret < 0 ? ret : EOF;
+
+    rule = *pr = calloc(1, sizeof(pfw_rule_t));
+    if (!rule)
+        return -ENOMEM;
+
+    word = pfw_context_take_word(ctx);
+    if (!word) {
+        PFW_DEBUG("Rule starts with NULL\n");
+        ret = -EINVAL;
+        goto err;
+    } else if (!strcmp(word, "ALL")) {
+        rule->predicate = PFW_PREDICATE_ALL;
+        branch = true;
+    } else if (!strcmp(word, "ANY")) {
+        rule->predicate = PFW_PREDICATE_ANY;
+        branch = true;
+    } else {
+        rule->criterion.def = word;
+        branch = false;
+    }
+
+    if (branch) {
+        /* Rule brances. */
+
+        pfw_context_take_line(ctx);
+        for (nb = 0;; nb++) {
+            ret = pfw_parse_rule(ctx, &sub, depth + 1);
+            if (ret == EOF)
+                break;
+            else if (ret < 0) {
+                PFW_DEBUG("Rule branch %d invalid\n", nb);
+                ret = -EINVAL;
+                goto err;
+            }
+
+            ret = pfw_vector_append(&rule->branches, sub);
+            if (ret < 0) {
+                ret = -ENOMEM;
+                goto err;
+            }
+        }
+    } else {
+        /* Rule leaves. */
+
+        word = pfw_context_take_word(ctx);
+        if (!word) {
+            PFW_DEBUG("Rule has no predicate\n");
+            ret = -EINVAL;
+            goto err;
+        } else if (!strcmp(word, "Is")) {
+            rule->predicate = PFW_PREDICATE_IS;
+        } else if (!strcmp(word, "IsNot")) {
+            rule->predicate = PFW_PREDICATE_ISNOT;
+        } else if (!strcmp(word, "Excludes")) {
+            rule->predicate = PFW_PREDICATE_EXCLUDES;
+        } else if (!strcmp(word, "Includes")) {
+            rule->predicate = PFW_PREDICATE_INCLUDES;
+        } else if (!strcmp(word, "In")) {
+            rule->predicate = PFW_PREDICATE_IN;
+        } else if (!strcmp(word, "NotIn")) {
+            rule->predicate = PFW_PREDICATE_NOTIN;
+        } else {
+            PFW_DEBUG("Rule use invalid predicate '%s'\n", word);
+            ret = -EINVAL;
+            goto err;
+        }
+
+        /* Parse state. */
+
+        word  = pfw_context_take_word(ctx);
+        if (!word) {
+            PFW_DEBUG("Rule has no state\n");
+            ret = -EINVAL;
+            goto err;
+        }
+
+        if (rule->predicate == PFW_PREDICATE_IN
+            || rule->predicate == PFW_PREDICATE_NOTIN) {
+            rule->state.itv = pfw_parse_interval(word);
+            if (!rule->state.itv) {
+                ret = -ENOMEM;
+                goto err;
+            }
+        } else {
+            rule->state.def = word;
+        }
+
+        pfw_context_take_line(ctx);
+    }
+
+    return 0;
+
+err:
+    pfw_free_rule(rule);
+    return ret;
+}
+
+static int pfw_parse_act(pfw_context_t* ctx, pfw_act_t** pa)
+{
+    pfw_act_t* act;
+    char* word;
+    int ret;
+
+    ret = pfw_context_get_depth(ctx);
+    if (ret != 2)
+        return ret < 0 ? ret : EOF;
+
+    act = *pa = calloc(1, sizeof(pfw_act_t));
+    if (!act)
+        return -ENOMEM;
+
+    /* plugin name. */
+
+    word = pfw_context_take_word(ctx);
+    if (!word) {
+        PFW_DEBUG("Act has no plugin name\n");
+        ret = -EINVAL;
+        goto err;
+    }
+
+    act->plugin.def = word;
+
+    word = pfw_context_take_word(ctx);
+    if (!word || strcmp(word, "=")) {
+        PFW_DEBUG("Act should use '=' instead of '%s'\n", word);
+        ret = -EINVAL;
+        goto err;
+    }
+
+    /* callback parameters. */
+
+    act->params = pfw_context_take_line(ctx);
+
+    return 0;
+
+err:
+    free(act);
+    return ret;
+}
+
+static int pfw_parse_config(pfw_context_t* ctx, pfw_config_t** pc)
+{
+    pfw_config_t* config;
+    pfw_rule_t* rule;
+    pfw_act_t* act;
+    char* word;
+    int ret, nb;
+
+    ret = pfw_context_get_depth(ctx);
+    if (ret != 1)
+        return ret < 0 ? ret : EOF;
+
+    config = *pc = calloc(1, sizeof(pfw_config_t));
+    if (!config)
+        return -ENOMEM;
+
+    /* config name. */
+
+    word = pfw_context_take_word(ctx);
+    if (!word || strcmp(word, "conf:")) {
+        PFW_DEBUG("Conf start with '%s'\n", word);
+        ret = -EINVAL;
+        return ret;
+    }
+
+    word = pfw_context_take_word(ctx);
+    if (!word) {
+        PFW_DEBUG("Conf has no name\n");
+        ret = -EINVAL;
+        return ret;
+    }
+
+    config->name = word;
+    pfw_context_take_line(ctx);
+
+    // rules.
+
+    ret = pfw_parse_rule(ctx, &rule, 2);
+    if (ret < 0 && ret != EOF) {
+        PFW_DEBUG("Conf '%s' uses invalid rules\n", config->name);
+        ret = -EINVAL;
+        return ret;
+    }
+
+    config->rules = rule;
+
+    // acts.
+
+    for (nb = 0;; nb++) {
+        ret = pfw_parse_act(ctx, &act);
+        if (ret == EOF)
+            break;
+        else if (ret < 0) {
+            PFW_DEBUG("Conf '%s' uses invalid act\n", config->name);
+            goto err;
+        }
+
+        ret = pfw_vector_append(&config->acts, act);
+        if (ret < 0)
+            goto err;
+    }
+
+    return 0;
+
+err:
+    pfw_free_config(config);
+    return ret;
+}
+
+static int pfw_parse_domain(pfw_context_t* ctx, pfw_domain_t** pd)
+{
+    pfw_domain_t* domain;
+    pfw_config_t* config;
+    char* word;
+    int ret, nb;
+
+    ret = pfw_context_get_depth(ctx);
+    if (ret < 0)
+        return ret;
+
+    domain = *pd = calloc(1, sizeof(pfw_domain_t));
+    if (!domain)
+        return -ENOMEM;
+
+    /* domain name. */
+
+    word = pfw_context_take_word(ctx);
+    if (!word || strcmp(word, "domain:")) {
+        PFW_DEBUG("Domain start with '%s'\n", word);
+        ret = -EINVAL;
+        goto err;
+    }
+
+    word = pfw_context_take_word(ctx);
+    if (!word) {
+        PFW_DEBUG("Domain has no name\n");
+        ret = -EINVAL;
+        goto err;
+    }
+
+    domain->name = word;
+    pfw_context_take_line(ctx);
+
+    /* configs. */
+
+    for (nb = 0;; nb++) {
+        ret = pfw_parse_config(ctx, &config);
+        if (ret == EOF)
+            break;
+        else if (ret < 0) {
+            PFW_DEBUG("Domain '%s' 's %dth config is invalid\n", domain->name, nb);
+            goto err;
+        }
+
+        ret = pfw_vector_append(&domain->configs, config);
+        if (ret < 0)
+            goto err;
+    }
+
+    return 0;
+
+err:
+    pfw_free_domain(domain);
+    return ret;
+}
+
+static int pfw_parse_criterion(pfw_context_t* ctx, pfw_criterion_t** pc)
+{
+    pfw_criterion_t* criterion;
+    char* word;
+    int ret, nb;
+
+    ret = pfw_context_get_depth(ctx);
+    if (ret < 0)
+        return ret;
+
+    criterion = *pc = calloc(1, sizeof(pfw_criterion_t));
+    if (!criterion)
+        return -ENOMEM;
+
+    /* criterion type. */
+
+    word = pfw_context_take_word(ctx);
+    if (!word) {
+        PFW_DEBUG("Criterion starts with NULL\n");
+        ret = -EINVAL;
+        goto err;
+    }
+    if (!strcmp(word, "NumericalCriterion")) {
+        criterion->type = PFW_CRITERION_NUMERICAL;
+    } else if (!strcmp(word, "ExclusiveCriterion")) {
+        criterion->type = PFW_CRITERION_EXCLUSIVE;
+    } else if (!strcmp(word, "InclusiveCriterion")) {
+        criterion->type = PFW_CRITERION_INCLUSIVE;
+    } else {
+        PFW_DEBUG("Criterion has invalid type '%s'\n", word);
+        ret = -EINVAL;
+        goto err;
+    }
+
+    /* criterion names. */
+
+    for (nb = 0;; nb++) {
+        word = pfw_context_take_word(ctx);
+        if (!word) {
+            PFW_DEBUG("Criterion has no ranges after %d names\n", nb);
+            ret = -EINVAL;
+            goto err;
+        } else if (!strcmp(word, ":"))
+            break;
+
+        ret = pfw_vector_append(&criterion->names, word);
+        if (ret < 0)
+            goto err;
+    }
+
+    /* criterion ranges. */
+
+    for (nb = 0;; nb++) {
+        word = pfw_context_take_word(ctx);
+        if (!word) {
+            if (nb == 0) {
+                PFW_DEBUG("Criterion has no ranges after ':'\n");
+                ret = -EINVAL;
+                goto err;
+            }
+            break;
+        } else if (!strcmp(word, "=")) {
+            word = pfw_context_take_word(ctx);
+            if (!word) {
+                PFW_DEBUG("Criterion has no value after '='\n");
+                ret = -EINVAL;
+                goto err;
+            }
+            criterion->init.def = (void*)word;
+            break;
+        }
+
+        if (criterion->type == PFW_CRITERION_NUMERICAL) {
+            pfw_interval_t* itv;
+
+            itv = pfw_parse_interval(word);
+            if (!itv) {
+                ret = -ENOMEM;
+                goto err;
+            }
+
+            ret = pfw_vector_append(&criterion->ranges, itv);
+            if (ret < 0) {
+                ret = -ENOMEM;
+                goto err;
+            }
+        } else {
+            if (criterion->type == PFW_CRITERION_INCLUSIVE && nb > 31) {
+                PFW_DEBUG("InclusiveCriterion's ranges has %d over 31\n", nb);
+                goto err;
+            }
+
+            ret = pfw_vector_append(&criterion->ranges, word);
+        }
+    }
+
+    pfw_context_take_line(ctx);
+    return ret;
+
+err:
+    pfw_free_criterion(criterion);
+    return ret;
+}
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+void pfw_free_settings(pfw_vector_t* settings)
+{
+    pfw_domain_t* domain;
+    int i;
+
+    for (i = 0; (domain = pfw_vector_get(settings, i)); i++)
+        pfw_free_domain(domain);
+
+    pfw_vector_free(settings);
+}
+
+int pfw_parse_settings(pfw_context_t* ctx, pfw_vector_t** p)
+{
+    pfw_domain_t* domain;
+    int ret, nb;
+
+    for (nb = 0;; nb++) {
+        ret = pfw_parse_domain(ctx, &domain);
+        if (ret == EOF)
+            break;
+        else if (ret < 0) {
+            PFW_DEBUG("Invalid %dth domain\n", nb);
+            return ret;
+        } else
+            pfw_vector_append(p, domain);
+    }
+
+    return nb;
+}
+
+void pfw_free_criteria(pfw_vector_t* criteria)
+{
+    pfw_criterion_t* criterion;
+    int i;
+
+    for (i = 0; (criterion = pfw_vector_get(criteria, i)); i++)
+        pfw_free_criterion(criterion);
+
+    pfw_vector_free(criteria);
+}
+
+int pfw_parse_criteria(pfw_context_t* ctx, pfw_vector_t** p)
+{
+    pfw_criterion_t* criterion;
+    int ret, nb;
+
+    for (nb = 0;; nb++) {
+        ret = pfw_parse_criterion(ctx, &criterion);
+        if (ret == EOF)
+            break;
+        else if (ret < 0) {
+            PFW_DEBUG("Invalid %dth criterion\n", nb);
+            return ret;
+        } else
+            pfw_vector_append(p, criterion);
+    }
+
+    return nb;
+}
